a62980837857148ed3189342dd421a5ff183b151
ivandrews557 <ireneandrews26@gmail.com>
2024-06-03 21:55:49 -0400

    Updated JS

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
-function americanPoem(event) {
-  event.preventDefault();
-
-  new Typewriter("#poem", {
-    strings: "Emily Dicken",
+function displayPoem(response){
+new Typewriter("#poem", {
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
 }
+
+
+
+
+
+
+
+function americanPoem(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+   let apiKey = "12fb5e8f183t409d043ba8bbea34o7b4";
+  let prompt =`Generate an American poem about ${instructionsInput.value}`;
+  let context="You are a nature expert and love to write short poems about nature. Generate a short poem in basic HTML";
+  let apiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
+}
+
 
 let poemFormElement = document.querySelector("#american-poem-form");
 poemFormElement.addEventListener("submit", americanPoem);
