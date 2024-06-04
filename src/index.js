939e164ac61825e9c944325eed4ce67cffe393d5
ivandrews557 <ireneandrews26@gmail.com>
2024-06-03 22:22:38 -0400

    changed the JS

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ function americanPoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
    let apiKey = "12fb5e8f183t409d043ba8bbea34o7b4";
   let prompt =`Generate an American poem about ${instructionsInput.value}`;
-  let context="You are a nature expert and love to write four lines of poem about nature. Generate a short poem in basic HTML. Sign the end of the poem with 'SheCodes AI' with a different color than the text.";
+  let context="You are a nature expert and love to write four lines of poem about nature. Generate a short poem in basic HTML. Sign the end of the poem with 'SheCodes AI' inside a <strong> element with a different color than the text.";
   let apiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayPoem);
